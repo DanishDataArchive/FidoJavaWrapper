0ec0271187cadded68b664028c82539972ea3b6d
Martin Jensby <marj@dda.dk>
2013-07-04 14:57:06 +0200

    Remove the auto generated build info file, and ignore it

--- src/dk/dda/version/BuildInfo.java
+++
@@ -1,8 +0,0 @@
-package dk.dda.version;
-
-// Auto-generated, triggered by project build
-public class BuildInfo {
-    public static String version = new String("0.1");
-    public static String date = new String("2013-07-04");
-    public static String time = new String("14:52:26");
-}
